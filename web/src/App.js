1543bf33843fd3b1087a803c6daa7d179b9b95a7
lukebelliveau <lbellive@thoughtworks.com>
2017-07-03 13:16:21 -0500

    web

---
+++ web/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Example from './example';
+
+class App extends Component {
+  render() {
+    return (
+      <Example />
+    );
+  }
+}
+
+export default App;
